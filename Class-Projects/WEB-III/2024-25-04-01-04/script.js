f2c741c594850c5241940098e8380f71c4b8ba9c
CadmanW <cadmanw13@gmail.com>
2024-09-16 14:22:50 -0500

    Bitmap to SVG web project is finished

--- Class-Projects/WEB-III/2024-25-04-01-04/script.js
+++ Class-Projects/WEB-III/2024-25-04-01-04/script.js
@@ -1,65 +1,384 @@
-const sqrSize = 100;
+const sqrSize = 30;
 
-// Wait for the window to load
-window.addEventListener("load", ()=>{
-    // for every key entry in the bitmaps object
-    Object.keys(bitmaps).forEach(key => {
+const bitmaps = {
+    a: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1]
+    ],
 
-        // get the bitmap itself
-        const bitmap = bitmaps[key];
+    b: [
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 0]
+    ],
 
-        // Make the SVG
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("width", bitmap[0].length * sqrSize);
-        svg.setAttribute("height", bitmap.length * sqrSize);
-        
-        //* Draw the squares in the SVG
-        // For every row in the bitmap
-        for (let row in bitmap) {
-            // For every entry in the row
-            for (let x in bitmap[row]) {
+    c: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
 
-                // Create the square
-                const sqr = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-                sqr.setAttribute("x", sqrSize * x);
-                sqr.setAttribute("y", sqrSize * row);
-                sqr.setAttribute("width", sqrSize);
-                sqr.setAttribute("height", sqrSize);
-                sqr.setAttribute("stroke", "#000000");
+    d: [
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 1, 1, 0]
+    ],
 
-                //* Figure out what color we fill the square with
-                let fillColor = null;
+    e: [
+        [1, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 1]
+    ],
 
-                // Check the length of the rows
-                switch (bitmaps[key][0].length) {
-                    case (3):
-                        fillColor = "#ffc000";
-                        break;
-                    case (4):
-                        // Make it a different color if the bitmap is representing a number
-                        if (Number.isInteger(parseInt(key))) {
-                            fillColor = "#70ad47";
-                        } else {
-                            fillColor = "#5b9bd5"
-                        }
-                        break;
-                    case (5):
-                        fillColor = "#ed7d31";
-                        break;
-                }
+    f: [
+        [1, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0]
+    ],
 
-                // If the current square of the bitmap holds 1, then fill the square
-                if (bitmap[row][x] === 1) {
-                    sqr.setAttribute("fill", fillColor);
-                } else {
-                    sqr.setAttribute("fill", "#ffffff");
-                }
+    g: [
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 1, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 1]
+    ],
 
-                // Add the square to the SVG
-                svg.appendChild(sqr);
+    h: [
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1]
+    ],
+
+    i: [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 1]
+    ],
+
+    j: [
+        [1, 1, 1, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [1, 1, 0, 0]
+    ],
+
+    k: [
+        [1, 0, 0, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 0],
+        [1, 0, 1, 0],
+        [1, 0, 0, 1]
+    ],
+
+    l: [
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 1]
+    ],
+
+    m: [
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1]
+    ],
+
+    n: [
+        [1, 0, 0, 1],
+        [1, 1, 0, 1],
+        [1, 0, 1, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1]
+    ],
+
+    o: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    p: [
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0]
+    ],
+
+    q: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 1, 1],
+        [0, 1, 1, 1]
+    ],
+
+    r: [
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1]
+    ],
+
+    s: [
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 1],
+        [1, 1, 1, 0]
+    ],
+
+    t: [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+
+    u: [
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1]
+    ],
+
+    v: [
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    w: [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 1]
+    ],
+
+    x: [
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1]
+    ],
+
+    y: [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0]
+    ],
+
+    z: [
+        [1, 1, 1, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 1]
+    ],
+
+    0: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    1: [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 1]
+    ],
+
+    2: [
+        [1, 1, 1, 0],
+        [0, 0, 0, 1],
+        [0, 1, 1, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 1]
+    ],
+
+    3: [
+        [1, 1, 1, 0],
+        [0, 0, 0, 1],
+        [0, 1, 1, 0],
+        [0, 0, 0, 1],
+        [1, 1, 1, 0]
+    ],
+
+    4: [
+        [1, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+    ],
+
+    5: [
+        [1, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 1],
+        [1, 1, 1, 0]
+    ],
+
+    6: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    7: [
+        [1, 1, 1, 1],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ],
+
+    8: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    9: [
+        [0, 1, 1, 0],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1],
+        [0, 0, 0, 1],
+        [0, 1, 1, 0]
+    ],
+
+    " ": [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+}
+
+
+
+
+
+
+
+
+
+function drawBitmap(key) {
+
+    // get the bitmap itself
+    const bitmap = bitmaps[key];
+
+    // Make the SVG
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", bitmap[0].length * sqrSize);
+    svg.setAttribute("height", bitmap.length * sqrSize);
+
+    //* Draw the squares in the SVG
+    // For every row in the bitmap
+    for (let row in bitmap) {
+        // For every entry in the row
+        for (let x in bitmap[row]) {
+
+            // Create the square
+            const sqr = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            sqr.setAttribute("x", sqrSize * x);
+            sqr.setAttribute("y", sqrSize * row);
+            sqr.setAttribute("width", sqrSize);
+            sqr.setAttribute("height", sqrSize);
+            sqr.setAttribute("stroke", "#000000");
+
+            //* Figure out what color we fill the square with
+            let fillColor = null;
+
+            // Check the length of the rows
+            switch (bitmaps[key][0].length) {
+                case (3):
+                    fillColor = "#ffc000";
+                    break;
+                case (4):
+                    // Make it a different color if the bitmap is representing a number
+                    if (Number.isInteger(parseInt(key))) {
+                        fillColor = "#70ad47";
+                    } else {
+                        fillColor = "#5b9bd5"
+                    }
+                    break;
+                case (5):
+                    fillColor = "#ed7d31";
+                    break;
             }
+
+            // If the current square of the bitmap holds 1, then fill the square
+            if (bitmap[row][x] === 1) {
+                sqr.setAttribute("fill", fillColor);
+            } else {
+                sqr.setAttribute("fill", "#ffffff");
+            }
+
+            // Add the square to the SVG
+            svg.appendChild(sqr);
         }
-        // Add the SVG to the DOM
-        document.body.appendChild(svg);
-    });
+    }
+    // Add the SVG to the DOM
+    document.body.appendChild(svg);
+}
+
+
+
+
+
+
+
+
+// Prompt for Input
+// Array.from(prompt().toLowerCase()).forEach(key => {drawBitmap(key)});
+
+document.addEventListener("keyup", keyup => {
+    const key = keyup.key.toLowerCase;
+    if (key in bitmaps.keys) {
+        drawBitmap(key);
+    }
 });
