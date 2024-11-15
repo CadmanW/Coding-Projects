987560883f076344c20b788ea276fc6ecacddfee
CadmanW <cadmanw13@gmail.com>
2024-11-15 14:58:36 -0600

    almost got it

--- Web-Development/Plinko Gambling/script.js
+++ Web-Development/Plinko Gambling/script.js
@@ -1,12 +1,6 @@
 const svg = document.querySelector("svg");
 
-initPlinkoBoard(
-    pegRadius = 50,
-    pegSpacing = 1,
-    pegColor = "black",
-    pegX = 10,
-    pegY = 10
-);
+initPlinkoBoard(200, 200, 25, 2);
 
 
 
@@ -16,16 +10,28 @@ initPlinkoBoard(
 
 
 
+function initPlinkoBoard(x, y, pegRadius, pegSpacing) {
 
-function initPlinkoBoard(pegRadius, pegSpacing, pegColor, pegX, pegY) {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    svg.setAttribute("viewbox", `0 0 ${x} ${y}`);
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("width", x);
+    svg.setAttribute("height", y);
+
+    const pegX = (x / pegRadius) + (pegRadius * pegSpacing);
+    const pegY = (x / pegRadius) + (pegRadius * pegSpacing);
+
     for (let y = 0; y < pegY; y++) {
         for (let x = 0; x < pegX; x++) {
             const peg = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-            peg.setAttribute("fill", pegColor);
+            peg.setAttribute("fill", "#181818");
+            peg.setAttribute("stroke", "#eee");
+            peg.setAttribute("stroke-width", "3px");
             /*
             Math for getting X and Y:
                 (pegRadius * x) gets the initial X coord for the circle
-                
+                (x * (pegSPacing * pegRadius)) gets the spacing between the pegs
+                + pegRadius moves it a little over so it's not clipping out of the
             */
 
             peg.setAttribute("cx", `${parseInt((pegRadius * x) + (x * (pegSpacing * pegRadius)) + pegRadius)}px`);
@@ -35,4 +41,6 @@ function initPlinkoBoard(pegRadius, pegSpacing, pegColor, pegX, pegY) {
             svg.appendChild(peg);
         }
     }
+
+    document.body.append(svg);
 }
