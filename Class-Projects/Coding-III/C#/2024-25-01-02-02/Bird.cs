d2228556e1f087d52891360957e17118338939fe
Cadmium <133188056+CadmanW@users.noreply.github.com>
2024-12-11 08:50:21 -0600

    yup

--- Class-Projects/Coding-III/C#/2024-25-01-02-02/Bird.cs
+++ Class-Projects/Coding-III/C#/2024-25-01-02-02/Bird.cs
@@ -8,15 +8,36 @@ namespace _2024_25_01_02_02
 {
     public class Bird
     {
-        private List<string> ValidBirds = new List<string>();
+        #region properties
 
+        private List<string> _validBirds = new List<string>();
+        public List<string> ValidBirds
+        {
+            get
+            {
+                return _validBirds;
+            }
+            private set
+            {
+                _validBirds = value;
+            }
+        }
         private string _type = "";
-
         public string Type
         {
-            get { return _type; }
-            set
-            {
+            get {
+                if (DateTime.Now.Hour != 12)
+                {
+                    MessageBox.Show("Cannot read this property until 12 :)");
+                    return null;
+                }
+                else
+                {
+                    return _type;
+                }
+              
+            }
+            set {
                 if (ValidBirds.Contains(value))
                 {
                     _type = value;
@@ -28,7 +49,10 @@ namespace _2024_25_01_02_02
             }
         }
 
-        // Constructor
+        #endregion
+
+        #region constructor
+
         public Bird()
         {
             ValidBirds.Add("Eastern Bluebird");
@@ -36,5 +60,6 @@ namespace _2024_25_01_02_02
             ValidBirds.Add("Red-Headed Woodpecker");
             Type = "";
         }
+        #endregion
     }
 }
