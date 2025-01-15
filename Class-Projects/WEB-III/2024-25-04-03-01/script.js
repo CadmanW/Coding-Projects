1573adb43d2902489b99e687dffb82a28e2200ac
Cadmium <133188056+CadmanW@users.noreply.github.com>
2025-01-15 11:36:50 -0600

    started

---
+++ Class-Projects/WEB-III/2024-25-04-03-01/script.js
@@ -0,0 +1,11 @@
+const mainEl = document.querySelector("main");
+const addBoxButtonEl = document.querySelector("add-box-button");
+const addItemButtonEl = document.querySelector("add-item-button");
+
+addBoxButtonEl.addEventListener("click", (e)=>{
+
+});
+
+addItemButtonEl.addEventListener("click", (e)=>{
+
+});
