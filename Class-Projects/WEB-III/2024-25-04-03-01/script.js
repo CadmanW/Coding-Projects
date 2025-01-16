fea80ab00b81f6c5fc624d2f9bd01ebebdd17957
CadmanW <cadmanw13@gmail.com>
2025-01-16 11:36:22 -0600

    modal no work

--- Class-Projects/WEB-III/2024-25-04-03-01/script.js
+++ Class-Projects/WEB-III/2024-25-04-03-01/script.js
@@ -1,6 +1,32 @@
+const modal = document.querySelector("dialog");
+
 // Event listeners for addBox and addItem
 document.querySelector("#add-box-button").addEventListener("click", createBox);
 document.querySelector("#add-item-button").addEventListener("click", addItem);
+
+// Creates a box
+function createBox() {
+    const boxNum = document.querySelectorAll(".box").length;
+
+    const boxEl = document.createElement("div");
+    boxEl.className = "box";
+    boxEl.id = boxNum;
+    boxEl.addEventListener("click", e=>selectBox(document.getElementById(e.target.ariaLabel))); // Whenever a box is clicked, the target returned as "e" is the superior child of said box, so I gave them all aria labels that let them "belong" to a box. Box is then selected using the aria label of it's child.
+
+    const titleEl = document.createElement("h1");
+    titleEl.classList.add("title");
+    titleEl.ariaLabel = boxNum;
+    titleEl.textContent = `Box #${boxNum}`;
+    
+    const itemListEl = document.createElement("ul");
+    itemListEl.classList.add("item-list");
+    itemListEl.ariaLabel = boxNum;
+
+    boxEl.appendChild(titleEl);
+    boxEl.appendChild(itemListEl);
+    document.querySelector("main").appendChild(boxEl);
+    selectBox(boxEl);
+}
 
 // Deselects old box and selects new box clicked
 function selectBox(box) {
@@ -10,23 +36,9 @@ function selectBox(box) {
     box.classList.add("selected");
 }
 
-// Creates a box
-function createBox() {
-    const boxes = document.querySelectorAll(".box").length;
-    const box = document.createElement("div");
-    box.className = "box";
-    box.id = boxes;
-    box.addEventListener("click", e=>selectBox(e.target));
-    document.querySelector("main").appendChild(box);
-    selectBox(box);
-}
-
 // Createa an item that belongs to the selected box
-async function addItem() {
-    const item = document.createElement("div");
+function addItem() {
+    const item = document.createElement("li");
     
-}
-
-function openModal() {
-    
+    modal.showModal();
 }
