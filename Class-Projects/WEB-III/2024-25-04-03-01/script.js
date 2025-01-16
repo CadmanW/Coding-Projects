c0586d02842be7e28c6a73968548c59fe43b8f95
Cadmium <133188056+CadmanW@users.noreply.github.com>
2025-01-16 08:50:46 -0600

    yup

--- Class-Projects/WEB-III/2024-25-04-03-01/script.js
+++ Class-Projects/WEB-III/2024-25-04-03-01/script.js
@@ -1,11 +1,17 @@
-const mainEl = document.querySelector("main");
-const addBoxButtonEl = document.querySelector("add-box-button");
-const addItemButtonEl = document.querySelector("add-item-button");
+document.querySelector("#add-box-button").addEventListener("click", createBox);
+document.querySelector("#add-item-button").addEventListener("click", addItem);
 
-addBoxButtonEl.addEventListener("click", (e)=>{
+function createBox() {
+    const boxes = document.querySelectorAll(".box").length;
+    const box = document.createElement("div");
+    box.className = "box";
+    box.id = boxes;
+    box.addEventListener("click", e=>e.target.classList.add("selected"));
+    document.querySelector("main").appendChild(box);
+    box.classList.add("selected");
+}
+// Make it deselect the old box after new box is selected
+function addItem() {
+    const item = document.createElement("div");
 
-});
-
-addItemButtonEl.addEventListener("click", (e)=>{
-
-});
+}
